a82cb08590adf030edd8006d4d623f976f03e0f5
rvenes <95510707+rvenes@users.noreply.github.com>
2026-02-06 00:20:54 +0100

    Use English log archive comments

---
+++ MeshtasticWin/Services/AppDataPaths.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace MeshtasticWin.Services;
+
+public static class AppDataPaths
+{
+    public static string BasePath =>
+        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "MeshtasticWin");
+
+    public static string LogsPath => Path.Combine(BasePath, "Logs");
+
+    public static string TraceroutePath => Path.Combine(LogsPath, "traceroute");
+
+    public static string GpsPath => Path.Combine(LogsPath, "gps");
+
+    public static void EnsureCreated()
+    {
+        Directory.CreateDirectory(BasePath);
+        Directory.CreateDirectory(LogsPath);
+        Directory.CreateDirectory(TraceroutePath);
+        Directory.CreateDirectory(GpsPath);
+        Debug.WriteLine($"MeshtasticWin BasePath: {BasePath}");
+    }
+}
